dda8776e41beb79a6c9e043edeeca75b05eaed4d
kaoala <34509099+kaoala7577@users.noreply.github.com>
2025-05-23 06:22:14 +0100

    Added team page Fixed some minor issues with JavaScript trying to query HTML elements not present in the current page

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const logosContainer = document.querySelector(".logos");
-const originalLogosNum = logosContainer.children.length;
+let originalLogosNum;
 let containerWidth = 0;
+if (logosContainer) {
+    originalLogosNum = logosContainer.children.length;
 
-document.addEventListener("DOMContentLoaded", () => {
-    cloneLogos(logosContainer);
-    scroll();
-});
+    document.addEventListener("DOMContentLoaded", () => {
+        cloneLogos(logosContainer);
+        scroll();
+    });
 
-window.addEventListener("resize", () => {
-    cloneLogos(logosContainer); // reclone on resize to adjust for new window size
-});
+    window.addEventListener("resize", () => {
+        cloneLogos(logosContainer); // reclone on resize to adjust for new window size
+    });
+}
 
 // logo scroll animation
 let scrollPosition = 0;
+
 function scroll() {
     scrollPosition -= 0.5; // adjust for scroll speed (lower=slower)
 
@@ -30,7 +34,7 @@ const cloneLogos = container => {
     const imgMargin = parseFloat(getComputedStyle(images[0]).marginRight) * 2; // margin on both sides
     const imgTotalWidth = imgWidth + imgMargin;
     containerWidth = imgTotalWidth * originalLogosNum;
-    
+
     // determine number of clones needed
     const cloneNum = Math.ceil(window.innerWidth / containerWidth);
     if (cloneNum * originalLogosNum + originalLogosNum === images.length) return; // don't need to clone if already cloned
@@ -40,7 +44,7 @@ const cloneLogos = container => {
     while (container.children.length > originalLogosNum) {
         container.removeChild(container.lastChild);
     }
-    
+
     // append new images
     for (let i = 0; i < cloneNum; i++) {
         images.forEach(logo => {
@@ -52,25 +56,61 @@ const cloneLogos = container => {
 
 // multi item carousel for team section
 const carousel = document.getElementById('carouselTeamIndicators');
-carousel.addEventListener('slide.bs.carousel', event => {
-    /*
-        CC 2.0 License Iatek LLC 2018 - Attribution required
-    */
-    const carouselInner = carousel.querySelector('.carousel-inner');
-    const items = Array.from(carousel.querySelectorAll('.carousel-item'));
-    const relatedTarget = event.relatedTarget;
-    const idx = items.indexOf(relatedTarget);
-    const itemsPerSlide = 3;
-    const totalItems = items.length;
+if (carousel) {
+    carousel.addEventListener('slide.bs.carousel', event => {
+        /*
+            CC 2.0 License Iatek LLC 2018 - Attribution required
+        */
+        const carouselInner = carousel.querySelector('.carousel-inner');
+        const items = Array.from(carousel.querySelectorAll('.carousel-item'));
+        const relatedTarget = event.relatedTarget;
+        const idx = items.indexOf(relatedTarget);
+        const itemsPerSlide = 3;
+        const totalItems = items.length;
 
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-        const it = itemsPerSlide - (totalItems - idx);
-        for (let i = 0; i < it; i++) {
-            if (event.direction === "left") {
-                carouselInner.appendChild(items[i]);
-            } else {
-                carouselInner.appendChild(items[0]);
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+            const it = itemsPerSlide - (totalItems - idx);
+            for (let i = 0; i < it; i++) {
+                if (event.direction === "left") {
+                    carouselInner.appendChild(items[i]);
+                } else {
+                    carouselInner.appendChild(items[0]);
+                }
             }
         }
-    }
-});
+    });
+}
+
+const teamModal = document.getElementById('teamModal');
+if (teamModal) {
+    teamModal.addEventListener('show.bs.modal', async event => {
+        const button = event.relatedTarget;
+        const recipient = button.getAttribute('data-bs-whatever');
+        const modalTitle = document.getElementById('teamModalTitle');
+        const modalDescription = document.getElementById('teamModalDescription');
+        const modalImage = document.getElementById('teamModalImage');
+
+        let newTitle;
+        let newDescription;
+        let newImage;
+        if (recipient === "daniel") {
+            newTitle = "Daniel Lecroy";
+            newDescription = "assets/text/Daniel.txt";
+            newImage = "assets/images/Daniel_Lecroy.jpg";
+        } else if (recipient === "li") {
+            newTitle = "Li Chang";
+            newDescription = "assets/text/Li Chang.txt";
+            newImage = "assets/images/Li Chang.PNG";
+        } else if (recipient === "rosa") {
+            newTitle = "Rosa DeCastello";
+            newDescription = "assets/text/Rosa_DeCastello.txt";
+            newImage = "assets/images/Rosa_DeCastello.jpg";
+        }
+        newDescription = await fetch(newDescription)
+            .then(text => text.text())
+            .then(text => text.replace(/\n/g, "<br>"));
+        modalTitle.textContent = newTitle + " - About me";
+        modalDescription.innerHTML = newDescription;
+        modalImage.src = newImage;
+    });
+}
